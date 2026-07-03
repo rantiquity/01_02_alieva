acd82fc5251d823aae63535bcbeee6a81352b932
locusclassicus <86204710+locusclassicus@users.noreply.github.com>
2026-07-04 01:14:26 +0300

    Refactor data processing in assignment.R

--- assignment.R
+++ assignment.R
@@ -1,23 +1,23 @@
-#install.packages("gutenbergr")
 library(gutenbergr)
 library(tidyverse)
 
 works <- gutenberg_works() 
- 
-# В каждом пункте используйте оператор pipe, не сохраняйте промежуточные результаты!
 
 # (1) Отберите ряды, в которых gutenberg_author_id равен 65 или 410;
 # после этого выберите два столбца: author, title
 my_data <- works |> 
+  filter(gutenberg_author_id %in% c(65, 410)) |> 
   select(author, title)
 
 # (2) Используйте функцию separate(), чтобы разделить 
 # столбец с именем и фамилией на два новых: author, name. 
 # Удалите столбец name
 my_data2 <- my_data |>
-  separate(author, into = c("author", "name"))
+  separate(author, into = c("author", "name"), sep = ", ") |> 
+  select(-name)
 
 # (3) Используйте group_by() и summarise(), чтобы узнать,
 # сколько произведений Шекспира и Марлоу хранится в библиотеке Gutenberg
-my_data3 <- my_data2 # |>
-  # ваш код здесь
+my_data3 <- my_data2 |>
+  group_by(author) |> 
+  summarise(n = n())
